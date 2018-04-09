851d064d86e4b1781a4f4bd6ac121589b5c29373
maciek1066 <maciek1066@gmail.com>
2018-04-09 23:17:10 +0200

    tst

---
+++ pyt_file.py
@@ -0,0 +1,12 @@
+class VoltageResistance(Resistor):
+    def __init__(self, ohms):
+        super().__init__(ohms)
+        self._voltage = 0
+    @property
+    def voltage(self):
+        return self._voltage
+
+    @voltage.setter
+    def voltage(self, voltage):
+        self._voltage = voltage
+        self.current = self._voltage / self.ohms
